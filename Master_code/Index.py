a9dfd1b51e9179a920a233618803612e41c4d40f
Elijah Ki-Zerbo <58334072+Waranika@users.noreply.github.com>
2024-07-04 22:28:36 +0100

    Add files via upload

--- Master_code/Index.py
+++ Master_code/Index.py
@@ -7,6 +7,7 @@ from skimage.color import rgb2gray
 from skimage import io, img_as_ubyte
 import matplotlib.pyplot as plt
 import numpy as np
+from TV import *
 
 # Function to generate a mask with a small square in the middle
 def generate_square_mask(image, square_size=50):
@@ -20,6 +21,7 @@ def generate_square_mask(image, square_size=50):
 original = io.imread(r"C:\Users\kizer\Master_code\input_0_zoom.png")
 gray_image = color.rgb2gray(original)
 
+
 # Generate a mask with a small square in the middle
 mask = generate_square_mask(gray_image, square_size=50)
 
@@ -31,14 +33,38 @@ masked_image[mask] = 0
 gray_image_rgb = np.stack([gray_image]*3, axis=-1)
 gray_image_rgb[mask] = [1, 0, 0]  # Red color for masked areas
 
+# Parameters
+lamda, gama = 0.1, 0.1
+iter = 1000
+
+# Run inpainting
+uk, rmse, num_iterations = tv1inpaint(masked_image, lamda, gama, iter, 214, 320)
+print(f'RMSE: {rmse[-1]}, Iterations: {num_iterations}')
+
+# Display the images using matplotlib
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+axes[0].imshow(masked_image, cmap='gray')
+axes[0].set_title('Original Grayscale Image')
+axes[0].axis('off')
+
+axes[1].imshow(gray_image_rgb)
+axes[1].set_title('Masked Image')
+axes[1].axis('off')
+
+axes[2].imshow(uk, cmap='gray')
+axes[2].set_title('Inpainted Image')
+axes[2].axis('off')
+
+plt.show()
+'''
 # Example CDD:
-p = 4
-# Define the g function based on curvature (e.g., g(s) = 1 / (1 + s) for s > 0)
+p = 5
+# Define the g function based on curvature 
 g = lambda s: s**p
 #g = lambda s: 1 / (1 + s)
-# Inpaint the image (assuming `image` and `mask` are defined)
-#ADD MASK TO IMAGE
-inpainted_image = cdd_inpainting(masked_image, g, iterations=5000, tau=1)
+# Inpaint the image 
+inpainted_image = cdd_inpainting(masked_image, mask, g, iterations=50000, tau=0.001)
 
 # Normalize the inpainted image to the range [0, 1]
 inpainted_image_normalized = (inpainted_image - np.min(inpainted_image)) / (np.max(inpainted_image) - np.min(inpainted_image))
@@ -58,6 +84,7 @@ else:
     print("The quality of the restored image is excellent")
 
 # Display the images using matplotlib
+
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 axes[0].imshow(masked_image, cmap='gray')
 axes[0].set_title('Original Grayscale Image')
@@ -72,3 +99,5 @@ axes[2].set_title('Inpainted Image')
 axes[2].axis('off')
 
 plt.show()
+
+'''
--- Master_code/TV.py
+++ Master_code/TV.py
@@ -1,24 +1,85 @@
 import numpy as np
+from scipy.sparse import spdiags
+from scipy.ndimage import convolve
 
-def TV(input_image, lambda_param, mask, T, dt):
-    i, j, _ = input_image.shape
-    mask = mask.astype(np.float64)
-    input_image = input_image.astype(np.float64)
-    input_image = mask * input_image + 255 * (1 - mask) * np.random.rand(*input_image.shape)
-    u = input_image.copy()
-    iterations = 0
+def u_grad(u, D1, D2):
+    x_grad = np.dot(u, D1)
+    y_grad = np.dot(D2, u)
+    return x_grad, y_grad
 
-    for t in np.arange(0, T, dt):
-        u_x, u_y = np.gradient(u)
-        N = np.sum(np.sqrt(u_x**2 + u_y**2))
-        u_xx, _ = np.gradient(u_x)
-        _, u_yy = np.gradient(u_y)
-        u_xy = (np.gradient(u_x, axis=0) + np.gradient(u_y, axis=1)) / 2
+def shrink(x_grad, y_grad, b_x, b_y, gama):
+    x1 = (x_grad + b_x) / (np.abs(x_grad + b_x) +1e-8)
+    m1 = np.abs(x_grad + b_x) - 1 / (gama + 1e-8) 
+    m1 = np.maximum(m1, 0)
+    d_x = x1 * m1
+    d_x[np.isnan(d_x)] = 0
 
-        deltaE = -(u_xx * u_y**2 - 2 * u_x * u_y * u_xy + u_yy * u_x**2) / (0.1 + (u_x**2 + u_y**2)**(3/2)) + 2 * mask * (lambda_param * (u - input_image))
-        u += dt * (-deltaE * N / (np.sqrt(np.sum(N**2))))
+    x2 = (y_grad + b_y) / (np.abs(y_grad + b_y) +1e-8)
+    m2 = np.abs(y_grad + b_y) - 1 / (gama + 1e-8) 
+    m2 = np.maximum(m2, 0)
+    d_y = x2 * m2
+    d_y[np.isnan(d_y)] = 0
 
-        iterations += 1
+    return d_x, d_y
 
-    u = u.astype(np.uint8)
-    return u, N
+def diff_col(y_data):
+    _, col = y_data.shape
+
+    y_data_rowless = np.hstack((y_data[:, 0:1], y_data[:, :-1]))
+    y_diff = y_data_rowless - y_data
+
+    return y_diff
+
+def diff_row(x_data):
+    row, _ = x_data.shape
+
+    x_data_rowless = np.vstack((x_data[0:1, :], x_data[:-1, :]))
+    x_diff = x_data_rowless - x_data
+
+    return x_diff
+
+def gau_sei(d_x, d_y, b_x, b_y, lamda, gama, u, uk, height, width):
+    D_prod = lamda * u
+    lamda_4g = lamda + 4 * gama
+
+    coef1 = D_prod / (lamda_4g + 1e-8)
+    coef2 = gama / (lamda_4g + 1e-8)
+
+    lapla = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+    uk_sum = convolve(np.pad(uk, ((1, 1), (1, 1)), 'reflect'), lapla, mode='constant')
+    uk_sum = uk_sum[1:height + 1, 1:width + 1]
+
+    db_sum = diff_row(d_x) + diff_col(d_y) - diff_row(b_x) - diff_col(b_y)
+
+    Gk = coef1 + coef2 * (uk_sum + db_sum)
+    return Gk
+
+def tv1inpaint(u, lamda, gama, iter, height, width):
+    global uk
+    uk = np.zeros((height, width))
+    b_x = np.zeros((height, width))
+    b_y = np.zeros((height, width))
+    e1 = np.ones(width)
+    e2 = np.ones(height)
+    D1 = spdiags([-e1, e1], [0, 1], width, width).toarray()
+    D1[width - 1, 0] = 1
+    D1 = D1.T
+    D2 = spdiags([-e2, e2], [0, 1], height, height).toarray()
+    D2[height - 1, 0] = 1
+
+    rmse = np.zeros(iter)
+
+    for num in range(iter):
+        x_grad, y_grad = u_grad(uk, D1, D2)
+        d_x, d_y = shrink(x_grad, y_grad, b_x, b_y, gama)
+
+        Gk = gau_sei(d_x, d_y, b_x, b_y, lamda, gama, u, uk, height, width)
+        dif = (uk - Gk) ** 2
+        uk = Gk
+
+        b_x = b_x + x_grad - d_x
+        b_y = b_y + y_grad - d_y
+
+        rmse[num] = np.sqrt(np.mean(dif))
+
+    return uk,rmse, num
